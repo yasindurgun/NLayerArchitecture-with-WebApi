65ecf3a99cdac62607c0b952d172d930519459e4
yasindurgun <yasin.durgun52@gmail.com>
2022-03-01 02:04:55 +0300

    Implement repository layer.

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayer.Core;
+using NLayer.Repository.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,5 +19,32 @@ namespace NLayer.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeature { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //reflection
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature()
+                {
+                    Id = 1,
+                    Color = "Kırmzıı",
+                    Height = 100,
+                    Width = 200,
+                    ProductId = 1
+                },
+                new ProductFeature()
+                {
+                    Id = 2,
+                    Color = "Mavi",
+                    Height = 300,
+                    Width = 300,
+                    ProductId = 2
+                }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
